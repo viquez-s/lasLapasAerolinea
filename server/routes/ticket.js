be84c9d1ba3eff0807922ceeb7eb002d0130a4a1
Steven Viquez <viquez.04.cr@gmail.com>
2021-11-10 19:49:09 -0600

    agregando routes

---
+++ server/routes/ticket.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/ticketController");
+
+//Autenticación para el uso del API
+const auth = require("../middleware/auth");
+const { permit } = require("../middleware/authorization");
+
+//Definición de rutas para cada uno de los verbos para los post
+//router.get("/", auth, permit("admin"), postController.get);
+router.get("/", auth, controller.get);
+
+router.get("/:id", auth, controller.getById);
+
+router.post("/", controller.create);
+
+router.delete("/:id", controller.delete);
+
+router.put("/:id", controller.update);
+
+module.exports = router;
