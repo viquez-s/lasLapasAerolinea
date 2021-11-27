4bc5bb52815abb5be7210787525655e6a508378f
anairenemarin <93504837+anairenemarin@users.noreply.github.com>
2021-11-26 21:51:20 -0600

    horarios

--- server/models/Route.js
+++ server/models/Route.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 const RouteSchema = new Schema({
   discountAmount: Number,
   duration: Number,
+  name: String,
   schedules: [{ type: Schema.Types.ObjectId, ref: "Schedule" }],
 });
 
