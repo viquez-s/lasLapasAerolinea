1553970b1587cca5a825fc61c5db133b24d43c37
Steven Viquez <viquez.04.cr@gmail.com>
2021-12-08 19:40:38 -0600

    actualización reserva

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,5 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FleetService} from "../../services/fleet.service";
+import {TicketService} from "../../services/ticket.service";
+import {PlaneService} from "../../services/planes.service";
+import {ScheduleService} from "../../services/schedule.service";
+
 
 @Component({
   selector: 'app-home',
@@ -8,19 +14,31 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
-
-
+  constructor(
+    private router: Router,
+    private activeRoute: ActivatedRoute,
+    private service: TicketService,
+    private PlaneService: PlaneService,
+    private FleetService: FleetService,
+    private ScheduleService: ScheduleService
+    ) {
   }
 
   form = new FormGroup({
     ida: new FormControl('', Validators.required),
     origen: new FormControl('', Validators.required),
-    destino: new FormControl(''),
-    salida: new FormControl(''),
     vuelta: new FormControl(''),
+    fleet: new FormControl('', [Validators.required]),
+    plane: new FormControl('', [Validators.required]),
+    schedule: new FormControl('', [Validators.required]),
   });
-  disableDestino = false;
+  list = '/ticket'
+  editMode = false;
+  model: any = {}
+  planesDropdown: any = [];
+  fleetsDropdown: any = [];
+  schedulesDropdown: any = [];
+  totalPrice: any = [];
 
   ngOnInit(): void {
 
@@ -40,6 +58,44 @@ export class HomeComponent implements OnInit {
       console.log('firstname value changed')
       console.log(x)
     })
+    this.activeRoute.params.subscribe((params) => {
+      if (params.id) {
+        this.editMode = true;
+
+        this.service.getById(params.id).subscribe((data: any) => {
+          this.model = data;
+          this.form.setValue({
+            seats: data.seats,
+            price: data.price,
+            fleet: data.fleet,
+            user: data.user,
+          });
+        });
+      }
+
+    });
+    this.PlaneService.get().subscribe((data) => {
+      console.log(data, 'data ')
+      this.planesDropdown = data;
+    });
+    this.FleetService.get().subscribe((data) => {
+      console.log(data, 'data ')
+      this.fleetsDropdown = data;
+    });
+    this.ScheduleService.get().subscribe((data) => {
+      console.log(data, 'data ')
+      this.schedulesDropdown = data;
+
+    });
+
+    this.form.get("schedules")?.valueChanges.subscribe(x => {
+      this.ScheduleService.getById(this.form.get("schedules")?.value).subscribe((data) => {
+      console.log(data, 'data ')
+      this.totalPrice = data.price * this.form.get("asientos")?.value;
+      });
+    })
+
   }
+
 
 }
